0160e475c754ba91a5b410da5088d5cc7ffa456c
aaronmao <aaronmao.hangzhou@gmail.com>
2024-06-12 23:12:42 +0000

    ioctl call test

---
+++ ioctl.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <libzfs_core.h>
+
+int main() {
+
+    if (libzfs_core_init() != 0) {
+        fprintf(stderr, "Can't init libzfs\n");
+        return 101;
+    }
+
+    lzc_mlec_test();
+
+    libzfs_core_fini();
+    
+    return 0;
+}
