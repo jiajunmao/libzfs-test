89113e38e7cffe24c26b9949ddaaf6bf4a8c9af5
aaronmao <aaronmao.hangzhou@gmail.com>
2024-06-13 01:34:47 +0000

    ioctl test call with binary data

--- ioctl.c
+++ ioctl.c
@@ -7,7 +7,9 @@
 #include <string.h>
 #include <sys/types.h>
 #include <libzfs_core.h>
-#include <libnvpair.h>
+#include <libzfs/sys/nvpair.h>
+
+typedef unsigned char uchar_t;
 
 int main() {
 
@@ -16,7 +18,29 @@ int main() {
         return 101;
     }
 
-    lzc_mlec_test();
+    // Define list
+    nvlist_t *nvl;
+    const char *key = "mybinarydata";
+    const char *data = "wutever";
+    size_t data_size = strlen(data) + 1;
+
+    // Allocate the list
+    if (nvlist_alloc(&nvl, NV_UNIQUE_NAME, 0) != 0) {
+        fprintf(stderr, "Failed to allocate nvlist\n");
+        return EXIT_FAILURE;
+    }
+
+    // Add binary data to the list
+    if (nvlist_add_byte_array(nvl, key, (uchar_t*) data, data_size) != 0) {
+        fprintf(stderr, "Failed to add binary data to nvlist\n");
+        nvlist_free(nvl);
+        return EXIT_FAILURE;
+    }
+
+    int ret = lzc_mlec_test("pool", nvl);
+    printf("lzc_mlec_test returned %d\n", ret);
+
+    nvlist_free(nvl);
 
     libzfs_core_fini();
     
