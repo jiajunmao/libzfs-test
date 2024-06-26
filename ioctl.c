547dc3173300aa1067fab8269dfba0c63388cfe4
Jiajun Mao <aaronmao.hangzhou@gmail.com>
2024-06-26 22:17:00 +0000

    Adding all required arguments

--- ioctl.c
+++ ioctl.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <libzfs_core.h>
-#include <libzfs/sys/nvpair.h>
+#include <libzfs/sys/nvpair.h>   
 
 typedef unsigned char uchar_t;
 
@@ -20,7 +20,7 @@ int main() {
 
     // Define list
     nvlist_t *nvl;
-    const char *key = "mybinarydata";
+    const char *key = "data";
     const char *data = "wutever";
     size_t data_size = strlen(data) + 1;
 
@@ -33,6 +33,21 @@ int main() {
     // Add binary data to the list
     if (nvlist_add_byte_array(nvl, key, (uchar_t*) data, data_size) != 0) {
         fprintf(stderr, "Failed to add binary data to nvlist\n");
+        nvlist_free(nvl);
+        return EXIT_FAILURE;
+    }
+
+    if (nvlist_add_uint64(nvl, "objset_id", 2)) {
+        nvlist_free(nvl);
+        return EXIT_FAILURE;
+    }
+
+    if (nvlist_add_uint64(nvl, "dn_object_id", 2)) {
+        nvlist_free(nvl);
+        return EXIT_FAILURE;
+    }
+
+    if (nvlist_add_uint64(nvl, "blk_id", 2)) {
         nvlist_free(nvl);
         return EXIT_FAILURE;
     }
